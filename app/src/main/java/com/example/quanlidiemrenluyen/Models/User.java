097e11ca300bcbc1ff4d3d3830a3f2f7bd1332a1
manhbv217 <manhbv217@gmail.com>
2019-03-22 16:47:24 +0700

    project dự án

---
+++ app/src/main/java/com/example/quanlidiemrenluyen/Models/User.java
@@ -0,0 +1,39 @@
+package com.example.uetpoint.Model;
+
+public class User {
+    private String username;
+    private String id;
+    private String imageURL;
+
+    public User(String userName, String id, String imageURL) {
+        this.username = userName;
+        this.id = id;
+        this.imageURL = imageURL;
+    }
+    public  User(){
+
+    }
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
